e3f7a4f701cf067ee6fbd6614222045277084e9b
Ali-Salman29 <ali.salman@arbisoft.com>
2024-09-17 20:02:13 +0200

    feat: add new settings for hosted-url
    - Add file-url argument for the hosted json files
    - Update README file
    - Fix issues in the Github action

--- prize_bond_service.py
+++ prize_bond_service.py
@@ -7,21 +7,25 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 from enum import Enum
 
+
 class PrizeBondStatus(Enum):
     """
     Prizebond status codes
     """
+
     WON = 1
     LOSE = 0
     NOT_FOUND = -1
+
 
 class PrizeBondService:
     """
     Service used to search prizebonds
     """
-    url = 'https://prizebond.result.pk/pb-ajax.php'
-    
-    def __init__(self, year, month, draw_values=['100', '200', '750', '1500']):
+
+    url = "https://prizebond.result.pk/pb-ajax.php"
+
+    def __init__(self, year, month, draw_values=["100", "200", "750", "1500"]):
         self.year = year
         self.month = month
         self.draw_values = draw_values
@@ -47,56 +51,56 @@ class PrizeBondService:
         dates_dict = {}
         for draw_str in draw_dates:
             try:
-                date_time_str = draw_str.split('/')[1]
-                draw_date = datetime.strptime(date_time_str, '%Y-%m-%d')
+                date_time_str = draw_str.split("/")[1]
+                draw_date = datetime.strptime(date_time_str, "%Y-%m-%d")
                 if draw_date.year in dates_dict:
                     dates_dict[draw_date.year][draw_date.month] = draw_str
                 else:
-                    dates_dict[draw_date.year] = {draw_date.month : draw_str}
+                    dates_dict[draw_date.year] = {draw_date.month: draw_str}
             except:
                 continue
         return dates_dict
 
     def _get_draw_dates(self, draw_value):
-            """
-            Call an API and returns all draws
-            Arguments:
-                draw_value: (str) i.e 100, 200, 750, 1500
-            Returns:
-                list of draws i.e ['01/2022-08-01', '03/2022-09-15', '03/2021-08-13']
-            """
-            draw_results = requests.post(self.url, data={'draw': draw_value})
-            soup = BeautifulSoup(draw_results.text, "html.parser")
-            dates = [option["value"] for option in soup.find_all("option")]
-            return dates
+        """
+        Call an API and returns all draws
+        Arguments:
+            draw_value: (str) i.e 100, 200, 750, 1500
+        Returns:
+            list of draws i.e ['01/2022-08-01', '03/2022-09-15', '03/2021-08-13']
+        """
+        draw_results = requests.post(self.url, data={"draw": draw_value})
+        soup = BeautifulSoup(draw_results.text, "html.parser")
+        dates = [option["value"] for option in soup.find_all("option")]
+        return dates
 
     def get_draws(self, year, month, draw_values):
-            """
-            Search all draws held in a month of a year
-            Arguments:
-                year: (int) i.e 2021, 2022
-                month: (int) values from 1-12
-                draw_values: list(str) draws to search in ['200', '750']
-            Returns:
-                {
-                    '750': '03/2022-09-15',
-                    '200': '03/2022-09-01''
-                }
-            """
-            current_draws = {}
-            for draw in draw_values:
-                draws = self._get_draw_dates(draw)
-                draws_dict = self._convert_draws_to_dict(draws)
-                if year in draws_dict and month in draws_dict[year]:
-                    current_draws[draw] = draws_dict[year][month]
-            return current_draws
+        """
+        Search all draws held in a month of a year
+        Arguments:
+            year: (int) i.e 2021, 2022
+            month: (int) values from 1-12
+            draw_values: list(str) draws to search in ['200', '750']
+        Returns:
+            {
+                '750': '03/2022-09-15',
+                '200': '03/2022-09-01''
+            }
+        """
+        current_draws = {}
+        for draw in draw_values:
+            draws = self._get_draw_dates(draw)
+            draws_dict = self._convert_draws_to_dict(draws)
+            if year in draws_dict and month in draws_dict[year]:
+                current_draws[draw] = draws_dict[year][month]
+        return current_draws
 
     def _parse_prizebond_response(self, response):
         """
         Parse response
         """
         soup = BeautifulSoup(response.text, "html.parser")
-        if len(soup.find_all('tr')) >= 3:
+        if len(soup.find_all("tr")) >= 3:
             return {"status": PrizeBondStatus.WON, "results": response.text}
         return {"status": PrizeBondStatus.LOSE, "results": response.text}
 
@@ -106,11 +110,11 @@ class PrizeBondService:
         """
         if draw_name in self.draws:
             pb_data = {
-                'number1': ', '.join(start),
-                'number2': ', '.join(end),
-                'pb_draw_detail': self.draws[draw_name],
-                'draw_name': draw_name,
+                "number1": ", ".join(start),
+                "number2": ", ".join(end),
+                "pb_draw_detail": self.draws[draw_name],
+                "draw_name": draw_name,
             }
-            response = requests.post(self.url, data = pb_data)
+            response = requests.post(self.url, data=pb_data)
             return self._parse_prizebond_response(response)
-        return {'status': PrizeBondStatus.NOT_FOUND, "results": None}
+        return {"status": PrizeBondStatus.NOT_FOUND, "results": None}
--- utils.py
+++ utils.py
@@ -1,10 +1,19 @@
-from jinja2 import Environment, FileSystemLoader
+"""
+Utils file
+"""
+import os
+import requests
 import smtplib, ssl
+import logging
+import json
+
+from jinja2 import Environment, FileSystemLoader
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-import logging
+
 
 log = logging.getLogger(__name__)
+
 
 def validate_user_credentials(username, password):
     """
@@ -16,6 +25,7 @@ def validate_user_credentials(username, password):
             server.login(username, password)
     except smtplib.SMTPAuthenticationError as error:
         raise error
+
 
 def generate_email_data(sender_email, draw_data):
     """
@@ -53,37 +63,38 @@ def generate_email_data(sender_email, draw_data):
                     "email": "abc@gmail.com",
                     "message": "<html>...</html>"
                 }
-            ]     
+            ]
     """
     email_data = []
     for draw_item in draw_data:
-        if draw_item.get('email'):
+        if draw_item.get("email"):
             message = MIMEMultipart("alternative")
             environment = Environment(loader=FileSystemLoader("templates/"))
-            if draw_item['body']:
+            if draw_item["body"]:
                 message["Subject"] = "Congratulations!"
                 template = environment.get_template("win.txt")
             else:
                 message["Subject"] = "Better Luck Next Time..."
                 template = environment.get_template("lose.txt")
             content = template.render(
-                name=draw_item['name'],
-                draw=draw_item['draw'],
-                date=draw_item['date'],
-                bond_details=draw_item['body']
+                name=draw_item["name"],
+                draw=draw_item["draw"],
+                date=draw_item["date"],
+                bond_details=draw_item["body"],
             )
             message["From"] = sender_email
-            message["To"] = draw_item['email']
-            message["CC"] = ','.join(draw_item['cc'])
+            message["To"] = draw_item["email"]
+            message["CC"] = ",".join(draw_item["cc"])
             mime_html = MIMEText(content, "html")
             message.attach(mime_html)
             message_data = {
-                'email': draw_item['email'],
-                'cc': draw_item['cc'],
-                'message': message.as_string()
+                "email": draw_item["email"],
+                "cc": draw_item["cc"],
+                "message": message.as_string(),
             }
             email_data.append(message_data)
     return email_data
+
 
 def send_mails(sender_email, sender_password, email_data):
     """
@@ -95,9 +106,30 @@ def send_mails(sender_email, sender_password, email_data):
         for message_data in email_data:
             try:
                 server.sendmail(
-                    sender_email, [message_data["email"]] + [message_data["cc"]], message_data['message']
+                    sender_email,
+                    [message_data["email"]] + [message_data["cc"]],
+                    message_data["message"],
                 )
                 log.info(f'Email sent to {message_data["email"]}')
             except:
                 log.warning(f'Error in sending email to {message_data["email"]}')
 
+
+def load_json(source):
+    """
+    load the data from the source, ther it's a local path or the
+    """
+    # Check if the source is a local file path
+    if os.path.isfile(source):
+        with open(source, "r") as file:
+            data = json.load(file)
+        return data
+    # Otherwise, assume it's a URL
+    else:
+        try:
+            response = requests.get(source)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching the JSON from URL: {e}")
+            return None
